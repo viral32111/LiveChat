7602f09237605c8542e9d89f9551d5c5c64f3ac0
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-01-06 19:43:28 +0000

    Create unit test for UUID helper function

--- Server/source/tests/unit/random.ts
+++ Server/source/tests/unit/random.ts
@@ -2,7 +2,7 @@
 import chai from "chai"
 
 // Import required functions from the helper script
-import { generateRandomString, generateRoomJoinCode } from "../../helpers/random"
+import { generateRandomString, generateRoomJoinCode, generateUUID } from "../../helpers/random"
 
 // Create a testing suite for testing the random functions
 suite( "Unit - Random", () => {
@@ -13,10 +13,16 @@ suite( "Unit - Random", () => {
 		chai.assert.equal( generateRandomString( 5, "A" ), "AAAAA", "Expected string to be 5 A's." )
 	} )
 
-	// Test input validation for the room names
+	// Test generating room join codes
 	test( "Room Join Code", () => {
 		chai.assert.lengthOf( generateRoomJoinCode(), 6, "Expected room join code to be 6 characters." )
 		chai.assert.notInclude( generateRoomJoinCode(), "1234567890", "Expected room join code to not contain numbers." )
+	} )
+
+	// Test generating UUIDs
+	test( "UUID", () => {
+		chai.assert.lengthOf( generateUUID(), 32, "Expected UUID to be 32 characters." )
+		chai.assert.notInclude( generateUUID(), "-", "Expected UUID to not contain hyphens." )
 	} )
 
 } )
